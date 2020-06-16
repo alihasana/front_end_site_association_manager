43a32473e0807d6aeaae8d467840c0251488ee24
gio1378 <gio13k@infonie.fr>
2020-06-16 19:48:18 +0200

    build for heroku

---
+++ build/precache-manifest.97e686d85ccc1d7566154bad2be925c4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95c4625499520aedc73f2107c627fea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56065338cdd868be8727",
+    "url": "/static/css/main.8c41f0a1.chunk.css"
+  },
+  {
+    "revision": "e5dea7476b63c54d59e9",
+    "url": "/static/js/2.73a6138a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.73a6138a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56065338cdd868be8727",
+    "url": "/static/js/main.2299c723.chunk.js"
+  },
+  {
+    "revision": "b26a6e11874742afa555",
+    "url": "/static/js/runtime-main.d286a094.js"
+  },
+  {
+    "revision": "c1811c02076211bd396e4524146cac01",
+    "url": "/static/media/PeleMeleAsso.c1811c02.png"
+  },
+  {
+    "revision": "e15852df5d2f457af365cdae8df18250",
+    "url": "/static/media/bg4.e15852df.jpeg"
+  },
+  {
+    "revision": "022300c109eceb8d971dc2a0761ed9dd",
+    "url": "/static/media/img_restos_du_coeur.022300c1.jpg"
+  },
+  {
+    "revision": "1e719472e9c7dad67eff1f6a30469c3e",
+    "url": "/static/media/logo512.1e719472.png"
+  },
+  {
+    "revision": "1dd6d3ec0e31c34e8fc49c1329b7fd0a",
+    "url": "/static/media/logo_restau_coeur.1dd6d3ec.svg"
+  },
+  {
+    "revision": "dd94a4e56b440370d1100ef9d3dcb71e",
+    "url": "/static/media/visual-easily-collaborate.dd94a4e5.png"
+  }
+]);
