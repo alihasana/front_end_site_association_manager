750ff2f5d7c10f9c7c54b29cf05bc360b1b4dd7f
alihasana <hasana.ali@gmail.com>
2020-07-30 02:08:26 +0200

    get the data with validation

--- src/views/Sections/ContactUsSection.js
+++ src/views/Sections/ContactUsSection.js
@@ -1,7 +1,8 @@
 import React,  { useState } from "react";
 import axios from "axios";
+import Joi from "@hapi/joi";
 // @material-ui/core components
-import {Checkbox, FormControlLabel, makeStyles, TextField} from "@material-ui/core";
+import {Checkbox, FormControlLabel, makeStyles} from "@material-ui/core";
 
 // @material-ui/icons
 // core components
@@ -11,7 +12,6 @@ import CustomInput from "components/CustomInput/CustomInput.js";
 
 import Button from "components/CustomButtons/Button.js";
 import styles from "assets/jss/material-kit-react/views/landingPageSections/contactUsStyle.js";
-import FormControl from "@material-ui/core/FormControl";
 
 const useStyles = makeStyles(styles);
 
@@ -19,30 +19,85 @@ export default function ContactUsSection(props) {
   const classes = useStyles();
   const {descriptionText, idUs} = props
   let [name, setName] = useState('');
+  let [nameError, setNameError] = useState('');
   let [email, setEmail] = useState('');
+  let [emailError, setEmailError] = useState('');
   let [message, setMessage] = useState('');
-  let [acceptCondition, setAcceptCondition] = useState(false);
+  let [messageError, setMessageError] = useState('');
+  let [acceptCondition, setAcceptCondition] = useState("");
+  let [acceptConditionError, setAcceptConditionError] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(name, email, message, acceptCondition)
-    try {
-      return await axios.get("/send-email/", {idUs});
-    } catch (error) {
-      return error;
+    console.log("name, email, message, acceptCondition");
+    console.log(name, email, message, acceptCondition);
+    if (acceptCondition) {
+      await setAcceptConditionError("");
+      await setAcceptCondition("true");
+    } else {
+      await setAcceptConditionError("Veuillez vous accepter condition, si vous souhaite continuer")
+      await setAcceptCondition("");
+    }
+    if (name && email && message && acceptCondition){
+      console.log('get all data');
+      try {
+        return await axios.get("/send-email/", {idUs});
+      } catch (error) {
+        return error;
+      }
     }
   }
-  const handleInput = data => {
-    let id = data.target.id
-    let value = data.target.value
-    if(id === 'message') setMessage(value);
-    if(id === 'name') setName(value);
-    if(id === 'email') setEmail(value);
-    if(id === 'acceptCondition'){
-      setAcceptCondition(data.target.checked);
-      console.log(data.target.checked)
+  const handleInput = async data => {
+    let inputId = data.target.id
+    let inputValue = data.target.value
+    if(inputId === 'message') {
+      let {error, value} = Joi.string().min(10).max(250).required().validate(inputValue);
+      if (error || value.replace(/<[^>]+>|\s/g, '') === ''){
+        await setMessageError("Le message est obligatoire. Le texte doit contenir moins de 250 caractères et plus de 10 caractères")
+        await setMessage("");
+      } else {
+        await setMessageError("");
+        await setMessage((value.replace(/<[^>]+>/g, '')).trim());
+      }
+    }
+    if(inputId === 'name') {
+      let {error, value} = Joi.string().min(2).max(50).required().validate(inputValue);
+      if (error || value.replace(/<[^>]+>|\s/g, '') === '') {
+        await setNameError("Le nom est obligatoire. Le texte doit contenir moins de 50 caractères et plus de 2 caractères")
+        await setName("");
+      } else {
+        await setNameError("");
+        await setName((value.replace(/<[^>]+>/g, '')).trim());
+      }
+    }
+    if(inputId === 'email') {
+      let {error, value} = Joi.string().email({ tlds: { allow: false } }).required().validate(inputValue);
+      if (error || value.replace(/<[^>]+>|\s/g, '') === '') {
+        await setEmailError("Le email est obligatoire")
+        await setEmail("");
+      } else {
+        await setEmailError("");
+        await setEmail((value.replace(/<[^>]+>/g, '')).trim());
+      }
+    }
+    if(inputId === 'acceptCondition'){
+      if (data.target.checked === true) {
+        await setAcceptConditionError("");
+        await setAcceptCondition("true");
+      } else {
+        await setAcceptConditionError("Veuillez vous accepter condition, si vous souhaite continuer")
+        await setAcceptCondition("");
+      }
     }
   };
+  const feedback = (errorMessage) => (
+      <div style={{color: '#db2269', fontSize: '15px', display: 'relative'}}>
+        {errorMessage}
+        <br/>
+        <br/>
+      </div>
+  );
   return (
     <div className={classes.section}>
       <GridContainer justify="center">
@@ -58,6 +113,7 @@ export default function ContactUsSection(props) {
                 <CustomInput
                   labelText="Votre Noms"
                   id="name"
+                  error={!(nameError === "")}
                   formControlProps={{
                     fullWidth: true
                   }}
@@ -65,22 +121,27 @@ export default function ContactUsSection(props) {
                     onChange: handleInput
                   }}
                 />
+                {nameError ? feedback(nameError) : ''}
               </GridItem>
               <GridItem xs={12} sm={12} md={6}>
                 <CustomInput
                   labelText="Votre Email"
                   id="email"
+                  error={!(emailError === "")}
                   formControlProps={{
                     fullWidth: true
                   }}
                   inputProps={{
+                    type:email,
                     onChange: handleInput
                   }}
                 />
+                {emailError ? feedback(emailError) : ''}
               </GridItem>
               <CustomInput
                 labelText="Votre Message"
                 id="message"
+                error={!(messageError === "")}
                 formControlProps={{
                   fullWidth: true,
                   className: classes.textArea
@@ -91,26 +152,25 @@ export default function ContactUsSection(props) {
                   onChange: handleInput
                 }}
               />
+                {messageError ? feedback(messageError) : ''}
               <GridItem xs={12} sm={12} md={12}>
-                <FormControlLabel
+              <FormControlLabel
                     control={
                       <Checkbox
+                          checked={!(acceptCondition === "")}
                           id="acceptCondition"
                           name="acceptCondition"
                           onChange={handleInput}
                           color="primary"
                       />
                     }
-
                     label="En utilisant ce formulaire, vous acceptez le stockage et la gestion de vos données par ce site *"
                 />
+                {acceptConditionError ? feedback(acceptConditionError) : ''}
               </GridItem>
-
-              <GridContainer justify="center">
-                <GridItem xs={12} sm={12} md={4} className={classes.textCenter}>
+                <GridItem xs={12} sm={12} md={4}>
                   <Button color="primary" type="submit">Envoyez</Button>
                 </GridItem>
-              </GridContainer>
             </GridContainer>
           </form>
         </GridItem>
