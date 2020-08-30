4beb3b8b8d9fcd3b7577da9670a581e4dc7d398a
NeslyPetitBert <petitbert.nesly@gmail.com>
2020-08-30 06:06:59 +0000

    biuld file

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97e686d85ccc1d7566154bad2be925c4.js"
+  "/precache-manifest.20b13afd3590a2fdb9e2889d4c707b7f.js"
 );
 
 self.addEventListener('message', (event) => {
